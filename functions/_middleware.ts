8485f0f5fbc169efb69fa887a8f90c28bcd10a02
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-06 15:45:36 -0700

    Update _middleware.ts

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,1 +1,22 @@
+import { createRemoteJWKSet, jwtVerify } from 'jose';
+
+const PROTECTED = [/^\/hub/];
+
+export const onRequest: PagesFunction = async (ctx) => {
+  const { request, env, next, cookies } = ctx;
+  const url = new URL(request.url);
+
+  if (!PROTECTED.some(r => r.test(url.pathname))) return next();
+
+  const access = cookies.get('allstar_at');
+  if (!access) return Response.redirect(new URL('/', url), 302);
+
+  const jwks = createRemoteJWKSet(new URL(env.JWKS_URL));
+  try {
+    await jwtVerify(access, jwks, { audience: 'allstar-agent-hub' });
+    return next();
+  } catch {
+    return Response.redirect(new URL('/', url), 302);
+  }
+};
 
