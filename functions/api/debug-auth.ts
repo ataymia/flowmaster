cde615b2572d3002b06fa6d90ea01ef160fe0561
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-06 17:47:36 -0700

    Update debug-auth.ts

--- functions/api/debug-auth.ts
+++ functions/api/debug-auth.ts
@@ -1,5 +1,2 @@
-export const onRequestGet: PagesFunction = async ({ env }) => {
-  return new Response(JSON.stringify({
-    AUTH_BASE: env.AUTH_BASE || null
-  }), { headers: { "content-type": "application/json" } });
-};
+export const onRequestGet: PagesFunction = async ({ env }) =>
+  new Response(JSON.stringify({ AUTH_BASE: env.AUTH_BASE || null }), { headers:{'content-type':'application/json'} });
