e138422d537a57558abff30b8f631bf3ea158698
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-07 16:35:59 -0700

    Update logout.ts

--- functions/api/logout.ts
+++ functions/api/logout.ts
@@ -1,4 +1,4 @@
-import { json, setCookie } from './_util';
+import { json, setCookie } from './_utils';
 export const onRequestPost: PagesFunction = async () => {
   const h = new Headers();
   h.append('Set-Cookie', setCookie('access_token','',{maxAge:0}));
