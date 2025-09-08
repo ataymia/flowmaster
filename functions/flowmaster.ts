94cee2821d695907267fa48052a4d2425eab63bd
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-07 19:01:24 -0700

    Create flowmaster.ts

---
+++ functions/flowmaster.ts
@@ -0,0 +1,5 @@
+// functions/flowmaster.ts
+export const onRequestGet: PagesFunction = async ({ request }) => {
+  const url = new URL('/flowmaster/index.html', request.url);
+  return fetch(url.toString(), request);
+};
