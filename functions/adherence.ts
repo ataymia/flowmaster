07e720409252466c46689f71a71537622c32cc88
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-07 19:38:53 -0700

    Update adherence.ts

--- functions/adherence.ts
+++ functions/adherence.ts
@@ -1,5 +1,18 @@
 // functions/adherence.ts
+function hasAuthCookie(req: Request) {
+  const c = req.headers.get('cookie') || '';
+  return /\b(access_token|refresh_token)=/.test(c);
+}
+
 export const onRequestGet: PagesFunction = async ({ request }) => {
-  const url = new URL('/adherence/index.html', request.url);
-  return fetch(url.toString(), request);
+  const url = new URL(request.url);
+
+  if (!hasAuthCookie(request)) {
+    url.pathname = '/';
+    url.search = '';
+    return Response.redirect(url.toString(), 302);
+  }
+
+  const fileUrl = new URL('/adherence/index.html', request.url);
+  return fetch(fileUrl.toString(), request);
 };
