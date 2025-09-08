16dca49059bd62c45223207d8040911791418f2a
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-08 15:16:02 -0700

    Update notion-check.ts

--- functions/api/notion-check.ts
+++ functions/api/notion-check.ts
@@ -1,13 +1,7 @@
-// functions/api/notion-check.ts
-type Env = { NOTION_SECRET: string; NOTION_DATABASE_ID: string };
-export const onRequestGet: PagesFunction<Env> = async ({ env }) => {
-  const ok = !!env.NOTION_SECRET && !!env.NOTION_DATABASE_ID;
-  return new Response(
-    JSON.stringify({
-      ok,
-      db_id: env.NOTION_DATABASE_ID || null,
-      secret_set: !!env.NOTION_SECRET,
-    }),
-    { headers: { "content-type": "application/json" } }
-  );
-};
+import { Env, json } from "./_utils";
+export const onRequestGet: PagesFunction<Env> = async ({ env }) =>
+  json({
+    ok: true,
+    has_secret: Boolean(env.NOTION_SECRET),
+    has_db: Boolean(env.NOTION_DATABASE_ID),
+  });
