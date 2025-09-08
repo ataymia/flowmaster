b6965aca2ea97bfed65e93ae15263bcfae73c686
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-08 15:08:45 -0700

    Delete functions/flowmaster.ts

--- functions/flowmaster.ts
+++
@@ -1,9 +0,0 @@
-// functions/flowmaster.ts
-export const onRequestGet: PagesFunction = async (ctx) => {
-  const url = new URL("/flowmaster/index.html", ctx.request.url);
-  const res = await ctx.env.ASSETS.fetch(new Request(url, ctx.request));
-  const out = new Response(res.body, res);
-  out.headers.set("Cache-Control", "no-store");
-  out.headers.append("Vary", "Cookie");
-  return out;
-};
