ebeeba44302b8a3f248b9049376ff0c3014cd1db
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-08 12:11:59 -0700

    Delete functions/hub.ts

--- functions/hub.ts
+++
@@ -1,9 +0,0 @@
-// Serve /public/hub.html with 200. No redirects.
-export const onRequestGet: PagesFunction = async (ctx) => {
-  const url = new URL("/hub.html", ctx.request.url);
-  const res = await ctx.env.ASSETS.fetch(new Request(url, ctx.request));
-  const out = new Response(res.body, res);
-  out.headers.set("Cache-Control", "no-store");
-  out.headers.append("Vary", "Cookie");
-  return out;
-};
