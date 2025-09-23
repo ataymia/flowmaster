f8105cb679e4efb77e776e296d3cb5245467de63
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-23 11:17:35 -0700

    Update logout.ts

--- functions/api/logout.ts
+++ functions/api/logout.ts
@@ -2,10 +2,19 @@
 import { Env, upstream, clearCookie, forwardSetCookies } from "./_utils";
 
 export const onRequestPost: PagesFunction<Env> = async ({ env }) => {
-  const res = await upstream(env, "/auth/logout", { method: "POST" });
+  // Try standard logout routes
+  const routes = ["/auth/logout", "/logout", "/auth/signout"];
+  let last: Response | null = null;
+
+  for (const r of routes) {
+    const res = await upstream(env, r, { method: "POST" });
+    last = res;
+    if (res.status < 500) break;
+  }
+
   const headers = new Headers();
-  forwardSetCookies(res, headers);
+  if (last) forwardSetCookies(last, headers);
   clearCookie(headers, "allstar_at", "/");
   clearCookie(headers, "allstar_rt", "/");
-  return new Response(null, { status: res.status, headers });
+  return new Response(null, { status: last ? last.status : 200, headers });
 };
