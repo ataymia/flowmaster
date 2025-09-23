4db56ae3a54400bcd95e8bfb121328d573e4b463
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-23 08:13:41 -0700

    Update change-password.ts

--- functions/api/change-password.ts
+++ functions/api/change-password.ts
@@ -1,5 +1,5 @@
-import { Env, proxyWithAuth } from "./_utils";
-
+import { Env, ensureAccess, proxyWithAuth } from "./_utils";
 export const onRequestPost: PagesFunction<Env> = async ({ request, env }) => {
-  return proxyWithAuth(request, env, "/auth/change-password", { method:"POST", body: request.body });
+  const g = ensureAccess(request); if (!g.ok) return g.response;
+  return proxyWithAuth(request, env, "/auth/change-password");
 };
