1beb505caa60d14752c2cbb2db8d1733f46354ed
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-24 05:11:43 -0700

    Update schedules.ts

--- functions/api/schedules.ts
+++ functions/api/schedules.ts
@@ -1,10 +1,11 @@
+// functions/api/schedules.ts
 import { Env, proxyWithAuth } from "./_utils";
 
 export const onRequest: PagesFunction<Env> = async ({ request, env }) => {
   if (request.method === "POST") {
-    return proxyWithAuth(request, env, "/schedules", { method: "POST", body: request.body });
+    return proxyWithAuth(request, env, "/schedules", { method:"POST", body: request.body });
   }
   const url = new URL(request.url);
   const qs = url.search ? url.search : "";
-  return proxyWithAuth(request, env, `/schedules${qs}`, { method: "GET" });
+  return proxyWithAuth(request, env, `/schedules${qs}`, { method:"GET" });
 };
