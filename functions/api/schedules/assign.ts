d226609d0b52f28a3055bfac9ca4b1cff9e89936
ataymia <ataymia.murray@allstarservicesnow.com>
2025-09-26 10:41:26 -0700

    Update assign.ts

--- functions/api/schedules/assign.ts
+++ functions/api/schedules/assign.ts
@@ -1,3 +1,3 @@
-import { type Env, proxyWithSession } from "./_utils";
+import { type Env, proxyWithSession } from "../_utils";
 export const onRequestPost: PagesFunction<Env> = ({ request, env }) =>
   proxyWithSession(request, env, "/schedules/assign", { method: "POST", body: request.body });
